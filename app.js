c0e524293f590e014e7c88a97f8d302720c4436d
Giovanne Dias <giovanneed@gmail.com>
2019-11-03 23:39:21 -0500

    allow requests

--- app.js
+++ app.js
@@ -85,6 +85,20 @@ app.post("/user", async (request, response) => {
 app.get("/user/:id", async (request, response) => {
     try {
         var user = await UserModel.findById(request.params.id).exec();
+
+         // Website you wish to allow to connect
+         response.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+        // Request methods you wish to allow
+        response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        response.setHeader('Access-Control-Allow-Credentials', true);
+
         response.send(user);
     } catch (error) {
         response.status(500).send(error);
